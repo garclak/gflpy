302eb77403f944d8a9d762098645e73026dd1930
garclak <gareth.meaker@gmail.com>
2015-07-28 06:32:53 +1000

    Started setting up the gflObjects

---
+++ gflObject/Aircraft.py
@@ -0,0 +1,27 @@
+class Aircraft(object):
+    def __init__(self):
+      self.id  = 0
+      self.Manufacturer  = ""
+      self.Model  = ""
+      self.Variant  = ""
+      self.Type  = 0
+      self.VMinOperating  = 0
+      self.VRotation  = 0
+      self.VClimb  = 0
+      self.VFlapsLimit  = 0
+      self.VLanding  = 0
+      self.MinRunwayLength  = 0
+      self.WeightTakeOffMax  = 0
+      self.WeightEmpty  = 0
+      self.WeightFuelMax  = 0
+      self.MaxPAXCount  = 0
+      self.AltitudeMax  = 0
+      self.MaxRangeNm  = 0
+      self.MaxEnduranceMinutes  = 0
+      self.EngineType  = 0
+      self.EngineCount  = 0
+      self.FuelCruisePPH  = 0
+      self.FuelMaxPPH  = 0
+      self.TotalFlightHours  = 0
+      self.MfgDate  = 0
+      self.Remarks  = ""		
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import gflConst
+import gflObject
 
 one = 1
 two = 2
@@ -12,3 +13,6 @@ print helloworld + "!" + " " + str(three)
 
 print "Piston Engine: " + str(gflConst.EngineC.PISTON)
 print "Admin Logon: " + str(gflConst.LogonC.ADMIN)
+
+ac1 = gflObject.Aircraft()
+print "ac1 ID: " + str(ac1.id)
